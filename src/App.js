3097e7218d7972a3165baea3c2e9c3a00aac3082
Alex <Talha>
2020-02-23 13:22:04 -0300

    32. Aplicando Class component

--- src/App.js
+++ src/App.js
@@ -1,35 +1,37 @@
-import React from 'react';
-//import logo from './logo.svg';
-import './App.css';
-import Social from './Social';
+import React, {Component} from 'react';
 
 
-const Equipe = (props) => {
-  return (
-    <Sobre nome={props.nome} cargo={props.cargo} idade={props.idade} />
-  )
+class Equipe extends Component{
+  
+  render(){
+    return (
+      <div>
+        <hr/>
+       <Sobre nome={this.props.nome} cargo={this.props.cargo} idade={this.props.idade} />
+      </div>
+    )
+  }
 }
-
-const Sobre = (props) => {
-  return (
-    
-    <div>
-      <h2>{props.nome}</h2>
-      <h3>{props.cargo}</h3>
-      <span>{props.idade}</span>
-    </div>
-  )
+class Sobre extends Component{
+  
+  render(){
+    return (
+      <div>
+        <span>Olá meu nome é {this.props.nome}</span> <br />
+        <span>Olá meu cargo é {this.props.cargo}</span><br />
+        <span>Olá minha idade é {this.props.idade}</span>
+      </div>
+    )
+  }
 }
-
 
 function App(props) {
   return (
-
-      <div>
-      
-        <h1>Conheça nossa equipe</h1>
-        <Equipe nome={props.nome}/>
-      </div>
+    <div>
+      <h1>Conheça nossa equipe</h1>
+      <Equipe nome="Alex" cargo="Analista de Sistemas" idade="36"/>
+      <Equipe nome="Aline" cargo="Jornalista" idade="37"/>
+    </div>
   )
   
 }
