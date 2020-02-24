82581f6b802b741bd583ff5329a76cac1bdfbcf6
Alex <Talha>
2020-02-24 10:28:49 -0300

    40. Manipulando formularios

--- src/App.js
+++ src/App.js
@@ -1,18 +1,45 @@
 import React, { Component } from 'react'
-import Membro from './components/Membro'
+
+
 class App extends Component {
 
   constructor(props){
     super(props);
     this.state = {
-  
+      email : "",
+      senha : "",
+      sexo : "masculino"
     }
   }
 
+
+  trocaSexo(e){
+    let trocaSexo = e.target.value
+    this.setState({sexo : trocaSexo})
+
+  }
+  
+  trocaEmail(e){
+
+    this.setState({sexo : e.target.value})
+
+  }
+  
+  
   render() {
     return (
       <div>
-        <Membro nome="Visitante"/>
+        <div>Login</div>
+        <label>Email </label>  <input type="text" name="email" value={this.state.email}  onChange={ (e) => this.setState({ email : e.target.value})}/> <br />
+         <label>Senha </label>  <input type="password" name="senha" value={this.state.senha} onChange={ (e) => this.setState({senha : e.target.value})} /> <br />
+        <select name="sexo" onChange={ (e) => this.trocaSexo(e)} value={this.state.sexo}>
+          <option value="masculino">Masculino</option>
+          <option value="feminino">Feminino</option>
+        </select>
+
+      <h3>Login {this.state.email}</h3>
+      <h3>Senha {this.state.senha}</h3>
+      <h3>Login {this.state.sexo}</h3>
       </div>
     )
   }
--- src/components/Membro/index.js
+++ src/components/Membro/index.js
@@ -10,32 +10,11 @@ class Membro extends Component {
         }
     }
 
-    entrar(){
-        this.setState({ status : true})
-    }
-
-    sair(){
-        this.setState({ status : false})
-    }
-
-
-
 
     render() {
         return (
             <div>
-                {this.state.status == 0 ?
-                <> 
-                    <h1>Ola Visitante</h1>
-                    <button onClick={ () => this.entrar() }>Entrar</button>
-                 </>
-                 :
-                 <> 
-                    <h1>Ola Alex</h1>
-                    <button onClick={ () => this.sair() }>Sair</button>
-                 </>
-
-                }    
+              
             </div>
             
         )
