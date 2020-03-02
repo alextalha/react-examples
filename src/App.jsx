466a749ae9a7688cc94f1e89acdb018c27e36d69
Alex <Talha>
2020-03-01 22:34:50 -0300

    todoList convertida para contextApi

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,18 @@
-import React, { useState } from 'react'
+import React from 'react'
+
+import TodoProvider from './contexts/TodoContext';
+import TodoList from './components/TodoList';
+import AddTodo from './components/AddTodo';
 
 
 const App = () => {
 
-    const  [todo, setTodo] = useState()
-
-    const [todos,setTodos] = useState([
-        {id:1 , title: 'Ir ao Mercado' , done:false },
-        {id:2 , title: 'Ir ao Mercado' , done:false },
-        {id:3 , title: 'Ir ao Mercado' , done:false }
-    ])
-
-    const handleFormSubmit = e => {
-        e.preventDefault()
-        setTodos([...todos, todo])
-
-    }
-
-    const handleInputChange = e => {
-
-        setTodo({
-            ...todo,
-            id : todos.length+1,
-            title: e.target.value,
-            done:false,
-        })
-    }
-
-
-
     return(
-        <div>{ todos.map( (i) => <div key={i.id}>{i.title}</div>)}
-        
-        <br />
-        <hr />
-        <br />
-
-        <form onSubmit={handleFormSubmit}>
-            <input onChange={handleInputChange} type="text" name="todo" id="title" placeholder="Nova Tarefa ..."/>
-            <button>Adicionar</button>
-        </form>
-        </div>
+      <TodoProvider>
+          <TodoList></TodoList>
+          <hr/>
+          <AddTodo></AddTodo>
+      </TodoProvider>
 
         )
     
